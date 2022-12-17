28e9517498970197355f2c7798f1d8963840456d
Vishwajeet Kumar <115339745+Vishu0123456@users.noreply.github.com>
2022-12-17 12:56:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,298 @@
+# 1. Create a list by picking an odd-index items from the first list and even index items from the second.
+
+###soln-
+
+list1 = [3, 6, 9, 12, 15, 18, 21]
+list2 = [4, 8, 12, 16, 20, 24, 28]
+res = list()
+
+odd_elements = list1[1::2]
+print("Element at odd-index positions from list one")
+print(odd_elements)
+
+even_elements = list2[0::2]
+print("Element at even-index positions from list two")
+print(even_elements)
+
+print("Printing Final third list")
+res.extend(odd_elements)
+res.extend(even_elements)
+print(res)
+
+# 2.Write a program to remove the item present at index 4 and add it to the 2nd position and at the end of the list.
+
+# soln-
+sample_list = [34, 54, 67, 89, 11, 43, 94]
+
+print("Original list ", sample_list)
+element = sample_list.pop(4)
+print("List After removing element at index 4 ", sample_list)
+
+sample_list.insert(2, element)
+print("List after Adding element at index 2 ", sample_list)
+
+sample_list.append(element)
+print("List after Adding element at last ", sample_list)
+
+# 3. Slice list into 3 equal chunks and reverse each chunk
+
+# soln-
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+print("Original list ", sample_list)
+
+length = len(sample_list)
+chunk_size = int(length / 3)
+start = 0
+end = chunk_size
+
+# run loop 3 times
+for i in range(3):
+    # get indexes
+    indexes = slice(start, end)
+
+    # get chunk
+    list_chunk = sample_list[indexes]
+    print("Chunk ", i, list_chunk)
+
+    # reverse chunk
+    print("After reversing it ", list(reversed(list_chunk)))
+
+    start = end
+    end += chunk_size
+
+# 4.  Count the occurrence of each element from a list
+
+#soln-
+
+sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+print("Original list ", sample_list)
+
+count_dict = dict()
+for item in sample_list:
+    if item in count_dict:
+        count_dict[item] += 1
+    else:
+        count_dict[item] = 1
+
+print("Printing count of each item  ", count_dict)
+
+
+# 5.Create a Python set such that it shows the element from both lists in a pair
+
+# soln-
+first_list = [2, 3, 4, 5, 6, 7, 8]
+print("First List ", first_list)
+
+second_list = [4, 9, 16, 25, 36, 49, 64]
+print("Second List ", second_list)
+
+result = zip(first_list, second_list)
+result_set = set(result)
+print(result_set)
+
+# 6.Find the intersection (common) of two sets and remove those elements from the first set
+# soln-
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+intersection = first_set.intersection(second_set)
+print("Intersection is ", intersection)
+for item in intersection:
+    first_set.remove(item)
+
+print("First Set after removing common element ", first_set)
+
+
+# 7.Checks if one set is a subset or superset of another set. If found, delete all elements from that set
+
+# soln-
+
+first_set = {57, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+print("First set is subset of second set -", first_set.issubset(second_set))
+print("Second set is subset of First set - ", second_set.issubset(first_set))
+
+print("First set is Super set of second set - ", first_set.issuperset(second_set))
+print("Second set is Super set of First set - ", second_set.issuperset(first_set))
+
+if first_set.issubset(second_set):
+    first_set.clear()
+
+if second_set.issubset(first_set):
+    second_set.clear()
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+# 8.Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
+# soln-
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon': 47, 'Emma': 69, 'Kelly': 76, 'Jason': 97}
+
+print("List:", roll_number)
+print("Dictionary:", sample_dict)
+
+# create new list
+roll_number[:] = [item for item in roll_number if item in sample_dict.values()]
+print("after removing unwanted elements from list:", roll_number)
+
+
+
+# 9.Get all values from the dictionary and add them to a list but don’t add duplicates
+
+# soln-
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53,
+         'july': 54, 'Aug': 44, 'Sept': 54}
+
+print("Dictionary's values - ", speed.values())
+
+speed_list = list()
+
+# iterate dict values
+for val in speed.values():
+    # check if value not present in a list
+    if val not in speed_list:
+        speed_list.append(val)
+print("unique list", speed_list)
+
+# 10.Remove duplicates from a list and create a tuple and find the minimum and maximum number
+
+# soln-
+sample_list = [87, 52, 44, 53, 54, 87, 52, 53]
+
+print("Original list", sample_list)
+
+sample_list = list(set(sample_list))
+print("unique list", sample_list)
+
+t = tuple(sample_list)
+print("tuple ", t)
+
+print("Minimum number is: ", min(t))
+print("Maximum number is: ", max(t))
+
+
+# 11.  Convert two lists into a dictionary
+
+#soln-
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+res_dict = dict(zip(keys, values))
+print(res_dict)
+
+# 12. Merge two Python dictionaries into one
+# soln-
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = {**dict1, **dict2}
+print(dict3)
+
+# 13.Print the value of key ‘history’ from the below dict
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+# soln-
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+# understand how to located the nested key
+# sampleDict['class'] = {'student': {'name': 'Mike', 'marks': {'physics': 70, 'history': 80}}}
+# sampleDict['class']['student'] = {'name': 'Mike', 'marks': {'physics': 70, 'history': 80}}
+# sampleDict['class']['student']['marks'] = {'physics': 70, 'history': 80}
+
+# solution
+print(sampleDict['class']['student']['marks']['history'])
+
+# 14. Initialize dictionary with default values
+# soln-
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+res = dict.fromkeys(employees, defaults)
+print(res)
+
+# Individual data
+print(res["Kelly"])
+
+# 15.Create a dictionary by extracting the keys from a given dictionary
+#soln-
+sampleDict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york" }
+
+keys = ["name", "salary"]
+
+newDict = {k: sampleDict[k] for k in keys}
+print(newDict)
+
+# 16. Delete a list of keys from a dictionary
+# soln-
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+sample_dict = {k: sample_dict[k] for k in sample_dict.keys() - keys}
+print(sample_dict)
+
+# 17. Check if a value exists in a dictionary
+# soln-
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+if 200 in sample_dict.values():
+    print('200 present in a dict')
+
+
+# 18. Rename key of a dictionary
+# soln-
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+sample_dict['location'] = sample_dict.pop('city')
+print(sample_dict)
+
+# 19.Change value of a key in a nested dictionary
+# soln-
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 6500}
+}
+
+sample_dict['emp3']['salary'] = 8500
+print(sample_dict)
